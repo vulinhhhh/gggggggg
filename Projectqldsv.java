b97224b39ebf08c51f29f993815892fcec86bb0d
linh <102957428+vulinhhhh@users.noreply.github.com>
2022-09-11 16:24:40 +0700

    Add files via upload
    add

---
+++ Projectqldsv.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package mainfile;
+import mainfile.dangnhap;
+/**
+ *
+ * @author kokomi
+ */
+public class Projectqldsv {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        dangnhap dn=new dangnhap();
+        dn.show();
+    }
+}
